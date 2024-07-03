ee41647905b6371d085ce83baef7d9b8460b435f
TheSumSt <2240765672@qq.com>
2024-07-03 13:51:09 +0800

    correct

--- 02Code/01_PythonProgram/Chapter1/02_01test.py
+++
@@ -1,3 +0,0 @@
-print("Welcome to Python")
-print("Welcome to Computer Science")
-print("Programming is fun")
--- 02Code/01_PythonProgram/Chapter1/02_03test.py
+++
@@ -1,5 +0,0 @@
-print("FFFFFF   U     U     NN    NN")
-print("FF       U     U     NNN   NN")
-print("FFFFFF   U     U     NN N  NN")
-print("FF        U   U      NN  N NN")
-print("FF         UUU       NN   NNN")
